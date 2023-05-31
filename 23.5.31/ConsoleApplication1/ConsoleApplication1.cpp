12d041bb4ae61347dcb09b2fcffe3e2d854f3f1a
DESKTOP-5S5CCIF\KGA_09 <sjbh3mc@gmail.com>
2023-05-31 11:11:20 +0900

    Create (23.5.31) first upload

---
+++ 23.5.31/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include<stdio.h>
+
+/*
+void Textout()
+{
+    printf("%d 를 출력합니다.\n", number);
+
+}
+*/ 
+int MultiplyThreeNumbers(int number1, int number2, int number3)
+{
+    printf("입력 값은 %d, %d, %d 입니다.", number1, number2, number3);
+    return number1 * number2 * number3;
+}
+
+int main()
+{
+    printf("세 수의 곱은 %d 입니다. \n\n\n", MultiplyThreeNumbers(3, 4, 5));
+
+
+}
---
+++ 23.5.31/sayname/sayname.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+
+int Name(int number)
+{
+	return number;
+}
+
+void PrintFunc(int age)
+{
+	printf("이대연, 나이: %d, 010-9063", age);
+}
+
+
+int main()
+{
+	printf("이름: 심준보\n");
+	printf("나이: %d세\n", Name(33));
+	printf("번호: 010-7355-7069\n");
+
+	PrintFunc(50); 
+
+
+}
